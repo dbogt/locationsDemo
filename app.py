5fa4b0cc6bec978b2760efe40c9ed9411fc07396
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-29 11:27:43 -0400

    shell locations

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ from streamlit_folium import st_folium
 
 df1 = pd.read_csv('locations1.csv')
 
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+m = folium.Map(location=[56.0659, -118.3917], zoom_start=16)
 folium.Marker(
   [39.949610, -75.150282], 
   popup="Liberty Bell", 
   tooltip="Liberty Bell"
 ).add_to(m)
 
+
+# Creating the map and adding points to it
+
+for index, location_info in df1.iterrows():
+    folium.Marker([location_info["lat"], location_info["lng"]], popup=location_info["name"]).add_to(map
 
 st.title("Gas Station Locations")
 # call to render Folium map in Streamlit
