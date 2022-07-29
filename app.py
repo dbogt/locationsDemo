7019e055a335e9554480c628c21813ce86a22cdd
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-07-29 11:59:10 -0400

    draw map

--- app.py
+++ app.py
@@ -5,21 +5,30 @@ from streamlit_folium import st_folium
 
 df1 = pd.read_csv('locations1.csv')
 
-m = folium.Map(location=[56.0659, -118.3917], zoom_start=16)
-# folium.Marker(
-#   [39.949610, -75.150282], 
-#   popup="Liberty Bell", 
-#   tooltip="Liberty Bell"
-# ).add_to(m)
+@st.cahce
+def drawMap():
+  m = folium.Map(location=[56.0659, -118.3917], zoom_start=16)
+  for index, location_info in df1.iterrows():
+    folium.Marker([location_info["lat"], location_info["lng"]], popup=location_info["name"], icon=folium.Icon(color="red")).add_to(m)
+  for index, location_info in df2.iterrows():
+    city =location_info['city']
+    address =location_info['address']
+    pop = """<b>Couche-Tard</b></br>
+    {}
+    """.format(address)
+    folium.Marker([location_info["latitude"], location_info["longitude"]], popup=pop, icon=folium.Icon(color="blue")).add_to(m)
 
+      
+  return m
 
-# Creating the map and adding points to it
-
-for index, location_info in df1.iterrows():
-    folium.Marker([location_info["lat"], location_info["lng"]], popup=location_info["name"]).add_to(m)
+m = drawMap()
 
 st.title("Gas Station Locations")
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width = 725)
+st_data = st_folium(m, width = 800)
 
+st.header("Shell Locations")
 st.dataframe(df1)
+
+st.header("Couche-Tard Locations")
+st.dataframe(df2)
